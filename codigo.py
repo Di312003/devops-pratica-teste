bc4beada49ac2427358f978283dbf6ec892b333f
Di312003 <127571048+Di312003@users.noreply.github.com>
2025-04-08 21:02:36 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,2 +1,2 @@
 # Lista com os valores de vendas realizadas em cada semana do mês
-vendas_mensais = [1000, 2000, 3000]
+vendas_mensais = [1000, 2000, 3000] #Total = 2400 (fora  da faixa de 5000 a 10000)
